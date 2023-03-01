0bc652a31789556793521888628bc961b0e14c42
Leo Volwahsen <volwahsenl@gmail.com>
2023-03-01 23:35:05 +0100

    sprachen werden hinzugefügt

--- portfolio-lv/src/App.jsx
+++ portfolio-lv/src/App.jsx
@@ -23,16 +23,25 @@ function App() {
                         <h5 className="skills-title">Kenntnisse</h5>
                         <ul className="skills-data">
                             <li>JavaScript: </li>
-                            <div className="skills-progressbar">70%</div>
+                            <div className="skills-progressbar1">80%</div>
                             <li>React: </li>
-                            <div className="skills-progressbar">60%</div>
+                            <div className="skills-progressbar2">85%</div>
                             <li>MongoDB:</li>
-                            <div className="skills-progressbar">70%</div>
-                            <li>Bootstrap:</li>
-                            <div className="skills-progressbar">50%</div>
+                            <div className="skills-progressbar3">80%</div>
+                            <li>NodeJS / Git:</li>
+                            <div className="skills-progressbar4">75%</div>
                             <li>SCSS/CSS/HTML:</li>
-                            <div className="skills-progressbar">80%</div>
+                            <div className="skills-progressbar5">85%</div>
                         </ul>
+                    </div>
+                    <div className="language-container">
+                        <h5 className="language-title">Sprachen</h5>
+                        <div className="circular-progress">
+                            <p className="progress-value">: Muttersprache</p>
+                        </div>
+                        <div className="circular-progress">
+                            <p className="progress-value">Englisch: C1</p>
+                        </div>
                     </div>
                 </div>
                 <div className="text"></div>
